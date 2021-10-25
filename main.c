7cee4d60f414351abd7389a21924f98a798f6d21
Han <76608239+hanpalla@users.noreply.github.com>
2021-10-25 11:38:07 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main ()
+{
+    int awal, akhir, a=0, b=1, c=0, batas=0, flag=0;
+    system("cls");
+    printf("Input batas awal\t: ");
+    scanf("%d",&awal);
+    printf("Input batas akhir\t: ");
+    scanf("%d",&akhir);
+
+    printf("Bilangan Fibonacci diantara %d dan %d adalah: ",awal,akhir);
+
+    if (a >= awal)
+    {
+        printf("%d ",a);
+    }
+    if (b >= awal)
+    {
+        printf("%d ",b);
+    }
+
+    while (batas<=akhir)
+    {
+        c=a+b;
+        
+        if (c >= awal && c <= akhir)
+        {
+            printf("%d ",c);
+            flag += 1;
+        }
+        batas=c;
+        a=b;
+        b=c;
+    }
+
+    if (flag == 0)
+        printf("Tidak ada");
+
+    return 0;
+}
